805eb451bd797904a596f991c9dd94b5ca2d56d1
manha-aziz20 <jinx29460@gmail.com>
2026-06-24 12:32:38 +0500

    if statement

---
+++ phase 2/if.py
@@ -0,0 +1,19 @@
+age=int(input("enter your age:"))
+if age < 18:
+    print("young")
+
+if age >= 18:
+    print("adult") 
+
+if age >= 50:
+    print("senior")
+
+password=str(input("enter your password"))
+if len(password) >= 8:
+    print("strong password")
+
+if len(password) < 8:
+    print("weak password")
+
+
+ 
